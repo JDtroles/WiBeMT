8548ed355a2ed4f7a44aae9d1d303d7f7e32763d
Dario <jonas.troles@gmail.com>
2020-04-01 12:39:57 +0200

    copy pasta code from fast text

--- main.py
+++ main.py
@@ -1,5 +1,19 @@
+# TODO: GloVe model (glove.840B.300d.zip)
+# TODO: word2vec GoogleNews model (Bolukbasi)
+
 
 import spacy
 import annoy
 import numpy
 
+# import io
+#
+#
+# def load_vectors(fname):
+#     fin = io.open(fname, 'r', encoding='utf-8', newline='\n', errors='ignore')
+#     n, d = map(int, fin.readline().split())
+#     data = {}
+#     for line in fin:
+#         tokens = line.rstrip().split(' ')
+#         data[tokens[0]] = map(float, tokens[1:])
+#     return data
