46318c4503d471b19fe158e989f019214915df8c
Dario <jonas.troles@gmail.com>
2020-04-23 17:48:05 +0200

    put list into lists.py, added getter

---
+++ lists.py
@@ -0,0 +1,56 @@
+def get_female_list_long():
+    return ["she", "hers", "her", "woman", "women", "mother", "female", "vulva"]
+
+
+def get_male_list_long():
+    return ["he", "his", "him", "man", "men", "father", "male", "penis"]
+
+
+def get_female_list_short():
+    return ["she"]
+
+
+def get_male_list_short():
+    return ["he"]
+
+
+def get_bolukbasi_female_list():
+    return ['her', 'she', 'women', 'woman', 'wife', 'mother', 'daughter', 'girls', 'girl', 'spokeswoman', 'female',
+            'sister', 'herself', 'actress', 'mom', 'girlfriend', 'daughters', 'lady', 'sisters', 'mothers',
+            'grandmother', 'ladies', 'queen', 'ma', 'wives', 'widow', 'bride', 'females', 'aunt', 'lesbian',
+            'chairwoman', 'moms', 'maiden', 'granddaughter', 'niece', 'hers', 'filly', 'princess', 'lesbians',
+            'actresses', 'maid', 'mare', 'fiancee', 'dads', 'waitress', 'maternal', 'heroine', 'nieces', 'girlfriends',
+            'mistress', 'womb', 'grandma', 'maternity', 'estrogen', 'widows', 'diva', 'nuns', 'nun', 'brides',
+            'housewife', 'menopause', 'motherhood', 'stepmother', 'hostess', 'fillies', 'congresswoman', 'witch',
+            'sorority', 'businesswoman', 'gal', 'schoolgirl', 'goddess', 'stepdaughter', 'uterus', 'mama', 'hens',
+            'hen', 'mommy', 'grandmothers', 'feminism', 'heiress', 'queens', 'witches', 'aunts', 'granddaughters',
+            'convent', 'vagina', 'maids', 'gals', 'housewives', 'obstetrics', 'councilwoman', 'matriarch', 'dowry',
+            'deer']
+
+
+def get_bolukbasi_male_list():
+    return ['he', 'his', 'him', 'man', 'men', 'spokesman', 'himself', 'son', 'father', 'guy', 'boy', 'boys', 'brother',
+            'male', 'brothers', 'dad', 'sons', 'king', 'businessman', 'grandfather', 'uncle', 'congressman', 'grandson',
+            'bull', 'businessmen', 'nephew', 'fathers', 'lads', 'lion', 'gentleman', 'fraternity', 'bachelor', 'bulls',
+            'prince', 'colt', 'salesman', 'dude', 'beard', 'councilman', 'gentlemen', 'stepfather', 'monks', 'lad',
+            'testosterone', 'nephews', 'daddy', 'kings', 'sir', 'stud', 'lions', 'gelding', 'czar', 'countrymen',
+            'penis', 'bloke', 'spokesmen', 'monastery', 'brethren', 'schoolboy', 'brotherhood', 'stepson', 'uncles',
+            'monk', 'viagra', 'macho', 'statesman', 'fathered', 'blokes', 'dudes', 'strongman', 'grandsons', 'studs',
+            'godfather', 'boyhood', 'baritone', 'grandpa', 'countryman', 'stallion', 'fella', 'chap', 'widower',
+            'salesmen', 'beau', 'beards', 'handyman', 'horsemen', 'fatherhood', 'princes', 'colts', 'fraternities',
+            'pa', 'fellas', 'councilmen', 'barbershop', 'fraternal', 'husband', 'boyfriend', 'males', 'husbands',
+            'sperm', 'fiance', 'suitors', 'prostate', 'paternity', 'suitor', 'hubby', 'boyfriends', 'semen']
+
+
+'''gender_list = ["lads", "lion", "gentleman", "fraternity", "bachelor", "niece", "bulls", "husbands", "prince",
+               "colt", "salesman", "hers", "dude", "beard", "filly", "princess", "lesbians", "councilman",
+               "actresses", "gentlemen", "stepfather", "monks", "lad", "sperm", "testosterone", "nephews",
+               "acknowledge", "acquaint", "acquiesce", "acquire", "acquit", "act", "activate", "actualize", "adapt",
+               "add", "address", "adhere", "adjoin", "adjourn", "adjudicate", "adjust", "administer",
+               "administrate", "admire", "admit", "admonish", "adopt", "adore", "adorn", "adsorb", "adulate",
+               "advance", "advertise"]'''
+
+
+'''bolukbasi_amiguous = ['deer', 'ma', 'husband', 'boyfriend', 'males', 'husbands', 'sperm', 'fiance', 'suitors',
+                      'prostate', 'paternity', 'suitor', 'hubby', 'boyfriends', 'semen']
+'''
--- main.py
+++ main.py
@@ -16,6 +16,7 @@ from pathlib import Path
 import os.path
 import tkinter as tk
 from tkinter import filedialog
+import lists as li
 
 INITIAL_DIR = "/home/jonas/Documents/GitRepos/Words"
 
@@ -357,53 +358,9 @@ if __name__ == "__main__":
     duration = time.time() - start_time
     print("You loaded the pickle in ", duration, " seconds!")
 
-    female_list_long = ["she", "hers", "her", "woman", "women", "mother", "female", "vulva"]
-    male_list_long = ["he", "his", "him", "man", "men", "father", "male", "penis"]
-    female_list = ["she"]
-    male_list = ["he"]
-
-    gender_list = ["lads", "lion", "gentleman", "fraternity", "bachelor", "niece", "bulls", "husbands", "prince",
-                   "colt", "salesman", "hers", "dude", "beard", "filly", "princess", "lesbians", "councilman",
-                   "actresses", "gentlemen", "stepfather", "monks", "lad", "sperm", "testosterone", "nephews",
-                   "acknowledge", "acquaint", "acquiesce", "acquire", "acquit", "act", "activate", "actualize", "adapt",
-                   "add", "address", "adhere", "adjoin", "adjourn", "adjudicate", "adjust", "administer",
-                   "administrate", "admire", "admit", "admonish", "adopt", "adore", "adorn", "adsorb", "adulate",
-                   "advance", "advertise"]
-
-    bolukbasi_male_list = ['he', 'his', 'him', 'man', 'men', 'spokesman', 'himself', 'son', 'father', 'guy', 'boy',
-                           'boys', 'brother', 'male', 'brothers', 'dad', 'sons', 'king', 'businessman', 'grandfather',
-                           'uncle', 'congressman', 'grandson', 'bull', 'businessmen', 'nephew', 'fathers',
-                           'lads', 'lion', 'gentleman', 'fraternity', 'bachelor', 'bulls', 'prince', 'colt', 'salesman',
-                           'dude', 'beard', 'councilman', 'gentlemen', 'stepfather', 'monks', 'lad', 'testosterone',
-                           'nephews', 'daddy', 'kings', 'sir', 'stud', 'lions', 'gelding', 'czar', 'countrymen',
-                           'penis', 'bloke', 'spokesmen', 'monastery', 'brethren', 'schoolboy', 'brotherhood',
-                           'stepson', 'uncles', 'monk', 'viagra', 'macho', 'statesman', 'fathered', 'blokes', 'dudes',
-                           'strongman', 'grandsons', 'studs', 'godfather', 'boyhood', 'baritone', 'grandpa',
-                           'countryman', 'stallion', 'fella', 'chap', 'widower', 'salesmen', 'beau', 'beards',
-                           'handyman', 'horsemen', 'fatherhood', 'princes', 'colts', 'fraternities', 'pa',
-                           'fellas', 'councilmen', 'barbershop', 'fraternal', 'husband', 'boyfriend', 'males',
-                           'husbands', 'sperm', 'fiance', 'suitors', 'prostate', 'paternity', 'suitor', 'hubby',
-                           'boyfriends', 'semen']
-    bolukbasi_female_list = ['her', 'she', 'women', 'woman', 'wife', 'mother', 'daughter', 'girls', 'girl',
-                             'spokeswoman', 'female', 'sister', 'herself', 'actress', 'mom', 'girlfriend', 'daughters',
-                             'lady', 'sisters', 'mothers', 'grandmother', 'ladies', 'queen', 'ma',
-                             'wives', 'widow', 'bride', 'females', 'aunt', 'lesbian', 'chairwoman', 'moms', 'maiden',
-                             'granddaughter', 'niece', 'hers', 'filly', 'princess', 'lesbians', 'actresses',
-                             'maid', 'mare', 'fiancee', 'dads', 'waitress', 'maternal', 'heroine',
-                             'nieces', 'girlfriends', 'mistress', 'womb', 'grandma', 'maternity', 'estrogen', 'widows',
-                             'diva', 'nuns', 'nun', 'brides', 'housewife', 'menopause', 'motherhood',
-                             'stepmother', 'hostess', 'fillies', 'congresswoman', 'witch',
-                             'sorority', 'businesswoman', 'gal', 'schoolgirl', 'goddess',
-                             'stepdaughter', 'uterus', 'mama', 'hens', 'hen', 'mommy', 'grandmothers',
-                             'feminism', 'heiress', 'queens', 'witches', 'aunts', 'granddaughters', 'convent',
-                             'vagina', 'maids', 'gals', 'housewives', 'obstetrics', 'councilwoman', 'matriarch',
-                             'dowry', 'deer']
-
-    bolukbasi_amiguous = ['deer', 'ma', 'husband', 'boyfriend', 'males', 'husbands', 'sperm', 'fiance', 'suitors',
-                          'prostate', 'paternity', 'suitor', 'hubby', 'boyfriends', 'semen']
 
     bolukbasi_female_list, bolukbasi_male_list = sort_bolukbasi_gender_list(
-        "/home/jonas/Documents/GitRepos/Words/GenderWordsBolukbasi.txt", female_list_long, male_list_long, pkl_dict)
+        "/home/jonas/Documents/GitRepos/Words/GenderWordsBolukbasi.txt", get_female_list_long(), get_male_list_long(), pkl_dict)
 
     print("Female list:")
     for elem in bolukbasi_female_list:
