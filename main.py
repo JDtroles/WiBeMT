992e6db3da27486d2eded7d0f874588f251a938d
Dario <jonas.troles@gmail.com>
2020-04-24 16:00:30 +0200

    finished splitting code into reader_saver.py and lists.py

--- main.py
+++ main.py
@@ -16,9 +16,9 @@ from pathlib import Path
 import os.path
 import tkinter as tk
 from tkinter import filedialog
-import lists as li
+import lists as word_lists
+import reader_saver
 
-INITIAL_DIR = "/home/jonas/Documents/GitRepos/Words"
 
 """Translates text into the target language.
 
@@ -118,79 +118,6 @@ def get_min_or_max_values(tuples, n, min):
         return tuples[(len(tuples) - n):]
 
 
-# TODO: fniish get_filename AND restructure all read functions
-def get_file_saver_instance(*file_type: str):
-    root = tk.Tk()
-    root.withdraw()
-    if file_type == ".txt":
-        # Load data (deserialize)
-        file_saver = filedialog.asksaveasfile("w", parent=root, initialdir=INITIAL_DIR, defaultextension=".txt")
-    else:
-        file_saver = filedialog.asksaveasfile("w", parent=root, initialdir=INITIAL_DIR)
-    return file_saver
-
-
-def get_file_path_for_saving(file_extension: str):
-    root = tk.Tk()
-    root.withdraw()
-
-    file_path = Path(filedialog.asksaveasfilename(parent=root, initialdir=INITIAL_DIR)).with_suffix(file_extension)
-    return file_path
-
-
-def get_file_path_for_loading():
-    root = tk.Tk()
-    root.withdraw()
-
-    file_path = Path(filedialog.askopenfilename(parent=root, initialdir=INITIAL_DIR))
-    return file_path
-
-
-def save_dict_to_pkl(dict):
-    file_path = get_file_path_for_saving(".pickle")
-    # Store data
-
-    if file_path is None:
-        print("Did not save:", file_path)
-        return
-    try:
-        with open(file_path, 'wb') as handle:
-            pickle.dump(dict, handle, protocol=pickle.HIGHEST_PROTOCOL)
-            print("Saved file to", file_path)
-        return
-    except FileExistsError:
-        print("Could not save dict to pickle")
-
-
-def load_pkl_to_dict():
-    file_path = get_file_path_for_loading()
-    # Load data
-    with open(file_path, 'rb') as handle:
-        unserialized_data = pickle.load(handle)
-    return unserialized_data
-
-
-def load_txt_to_dict(path):
-    embeddings_dict = {}
-    with open(path, 'r', encoding="utf-8") as f:
-        for line in tqdm(f.readlines(), desc="Creating dictionary: "):
-            values = line.split(" ")
-            word = values[0]
-            vector = np.asarray(values[1:], "float32")
-            embeddings_dict[word] = vector
-    return embeddings_dict
-
-
-def load_vocab_to_list(path):
-    vocab_list = []
-    with open(path, 'r', encoding="utf-8") as f:
-        for line in tqdm(f.readlines(), desc="Creating vocab list: "):
-            values = line.split(" ")
-            word = values[1]
-            vocab_list.append(word.rstrip())
-    return vocab_list
-
-
 def sort_bolukbasi_gender_list(gender_list_path, fem_list, ma_list, word_emb):
     gender_list_female = []
     gender_list_male = []
@@ -226,14 +153,14 @@ def get_occupations(path) -> list:
     return occupations_list
 
 
-def get_sentences(path_sentences, occu_list):
+def get_unique_sentences(path_sentences, occupations_list):
     sentences_set = set()
     with open(path_sentences, 'r', encoding="utf-8") as f:
         for line in f:
             values = line.split("\t")
             occupation = values[3].replace("the ", "").replace("The ", "").replace("a ", "").replace("an ", "").rstrip()
             sentence = values[2].replace(occupation, "X")
-            for elem in occu_list:
+            for elem in occupations_list:
                 sentence = sentence.replace(elem, "Y")
             # she, he = X1; her, his = X2; her, him = X3
             gender_pronoun_space = [" she ", " he ", " her ", " him ", " his "]
@@ -282,71 +209,23 @@ def add_adj_to_sentences(path_sentences, adj_list):
     print("Zeichen:", n_of_letters)
 
 
-def write_list_to_file(list_to_save, nested_bool):
-    file_saver = get_file_saver_instance(".txt")
-    if file_saver is None:
-        print("Not Saved")
-        return
-    for item in list_to_save:
-        if not nested_bool:
-            file_saver.write(str(item))
-        else:
-            for i, value in enumerate(item):
-                file_saver.write(str(value))
-                if i+1 < len(item):
-                    file_saver.write("\t")
-        file_saver.write("\n")
-    print("File saved")
-    '''
-    while True:
-        filename = input("Enter a file name: ")
-        try:
-            if not Path(file_path).is_file():
-                break
-        except FileExistsError:
-            print("There already is a file with this filename.")
-            continue
-    print("Filename is:", filename)
 
-    file_to_open = data_folder / filename
-    with open(file_to_open, "w") as f:
-        for item in list_to_save:
-            if not nested_bool:
-                f.write(item, "\n")
-            else:
-                for value in item:
-                    f.write(value, "\t")
-                f.write("\n")
-    '''
-    # print(filename)
 
 
 if __name__ == "__main__":
-    save_dict_to_pkl({1})
-    write_list_to_file([[1,2,3,4,5,6],["a", "b", "c"], ["trallalala"]], True)
+    reader_saver.save_dict_to_pkl({1})
+    reader_saver.write_nested_list_to_file([[1,2,3,4,5,6],["a", "b", "c"], ["trallalala"]])
     occupations_list_WinoBias = get_occupations("/home/jonas/Documents/GitRepos/Words/WinoBias.txt")
-    get_sentences("/home/jonas/Documents/GitRepos/Words/WinoBias.txt", occupations_list_WinoBias)
+    get_unique_sentences("/home/jonas/Documents/GitRepos/Words/WinoBias.txt", occupations_list_WinoBias)
     # get_occupations("/home/jonas/Documents/GitRepos/Words/Sentences_Occupations_Stanovsky.txt")
 
     add_adj_to_sentences("/home/jonas/Documents/GitRepos/Words/WinoBias.txt",
                          ["sassy", "brunette", "gorgeous", "grizzled", "burly", "scruffy"])
 
-    adjectives_list = load_vocab_to_list("/home/jonas/Documents/GitRepos/Words/Adjectives.txt")
-    verbs_list = load_vocab_to_list("/home/jonas/Documents/GitRepos/Words/Verbs.txt")
-
-    print(len(adjectives_list))
-    print(adjectives_list[0:10])
-    print(len(verbs_list))
-    print(verbs_list[0:10])
-
-    # glove.840B = 2196017
-    # glove.6B = 400000
+    adjectives_list = reader_saver.load_vocab_to_list()
+    verbs_list = reader_saver.load_vocab_to_list()
 
     # embeddings_dict = load_txt_to_dict("/home/jonas/Documents/GitRepos/PretrainedWordVectors/crawl-300d-2M.vec")
-
-    pkl_path_6B = "/home/jonas/Documents/GitRepos/PretrainedWordVectors/glove.6B.300d.pickle"
-    pkl_path_840B = "/home/jonas/Documents/GitRepos/PretrainedWordVectors/glove.840B.300d.pickle"
-    pkl_path_crawl = "/home/jonas/Documents/GitRepos/PretrainedWordVectors/crawl.?.300d.pickle"
 
     start_time = time.time()
     # save_dict_to_pkl(embeddings_dict, pkl_path_crawl)
@@ -354,56 +233,9 @@ if __name__ == "__main__":
     print("You dumped the pickle in ", duration, " seconds!")
 
     start_time = time.time()
-    pkl_dict = load_pkl_to_dict()
+    pkl_dict = reader_saver.load_pkl_to_dict()
     duration = time.time() - start_time
     print("You loaded the pickle in ", duration, " seconds!")
-
-
-    bolukbasi_female_list, bolukbasi_male_list = sort_bolukbasi_gender_list(
-        "/home/jonas/Documents/GitRepos/Words/GenderWordsBolukbasi.txt", get_female_list_long(), get_male_list_long(), pkl_dict)
-
-    print("Female list:")
-    for elem in bolukbasi_female_list:
-        string = "'" + elem + "'"
-        print(string.strip(), end=", ")
-
-    print("\n", "Male list: ")
-    for elem in bolukbasi_male_list:
-        string = "'" + elem + "'"
-        print(string.strip(), end=", ")
-
-    with open("/home/jonas/Documents/GitRepos/Words/GenderWordsBolukbasi.txt", 'r', encoding="utf-8") as f:
-        bolukbasi_base = []
-        for line in tqdm(f.readlines(), desc="Creating vocab list: "):
-            values = line.split(", ")
-            for word in values:
-                bolukbasi_base.append(word)
-
-    print("\n", "Bolukbasi list: \n", evaluate_words_for_gender(bolukbasi_base, male_list, female_list, pkl_dict, True))
-
-    evaluated_adjectives = evaluate_words_for_gender(adjectives_list, bolukbasi_male_list, bolukbasi_female_list,
-                                                     pkl_dict, True)
-    evaluated_verbs = evaluate_words_for_gender(verbs_list, bolukbasi_male_list, bolukbasi_female_list, pkl_dict, True)
-    evaluated_occupations_WinoBias = evaluate_words_for_gender(occupations_list_WinoBias, bolukbasi_male_list,
-                                                               bolukbasi_female_list, pkl_dict, True)
-    print("Female Occupations: ")
-    for word in get_min_or_max_values(evaluated_occupations_WinoBias, 20, True):
-        print(word)
-    print("Male Occupations: ")
-    for word in get_min_or_max_values(evaluated_occupations_WinoBias, 20, False):
-        print(word)
-    print("Female verbs: ")
-    for word in get_min_or_max_values(evaluated_verbs, 20, True):
-        print(word)
-    print("Male verbs: ")
-    for word in get_min_or_max_values(evaluated_verbs, 20, False):
-        print(word)
-    print("Female adjectives: ")
-    for word in get_min_or_max_values(evaluated_adjectives, 20, True):
-        print(word)
-    print("Male adjectives: ")
-    for word in get_min_or_max_values(evaluated_adjectives, 20, False):
-        print(word)
 
 
     '''
---
+++ reader_saver.py
@@ -0,0 +1,123 @@
+from tqdm import tqdm
+import pickle
+from pathlib import Path
+import tkinter as tk
+from tkinter import filedialog
+import numpy as np
+
+
+INITIAL_DIR = "/home/jonas/Documents/GitRepos/Words"
+
+
+# TODO: finish get_filename AND restructure all read functions
+def get_file_saver_instance(file_type: str = None):
+    root = tk.Tk()
+    root.withdraw()
+
+    if file_type == ".txt":
+        # Load data (deserialize)
+        file_saver = filedialog.asksaveasfile("w", parent=root, initialdir=INITIAL_DIR, defaultextension=".txt")
+    else:
+        file_saver = filedialog.asksaveasfile("w", parent=root, initialdir=INITIAL_DIR)
+
+    return file_saver
+
+
+def get_file_path_for_saving(file_extension: str = None) -> str:
+    root = tk.Tk()
+    root.withdraw()
+
+    if file_extension is not None:
+        file_path = Path(filedialog.asksaveasfilename(parent=root, initialdir=INITIAL_DIR)).with_suffix(file_extension)
+    else:
+        file_path = Path(filedialog.asksaveasfilename(parent=root, initialdir=INITIAL_DIR))
+
+    path = file_path.absolute().as_posix()
+    return path
+
+
+def get_file_path_for_loading(window_title) -> str:
+    root = tk.Tk()
+    root.withdraw()
+
+    file_path = Path(filedialog.askopenfilename(parent=root, initialdir=INITIAL_DIR, title=window_title))
+    path = file_path.absolute().as_posix()
+
+    return path
+
+
+def save_dict_to_pkl(dict_to_save) -> None:
+    file_path = get_file_path_for_saving(".pickle")
+
+    # Store data
+
+    if file_path is None:
+        print("Did not save:", file_path)
+        return
+    try:
+        with open(file_path, 'wb') as handle:
+            pickle.dump(dict_to_save, handle, protocol=pickle.HIGHEST_PROTOCOL)
+            print("Saved file to", file_path)
+        return
+    except OSError:
+        print('cannot save: ', file_path)
+
+
+def load_pkl_to_dict() -> dict:
+    file_path = get_file_path_for_loading("Choose a word embedding file in .pickle format")
+    # Load data
+    try:
+        with open(file_path, 'rb') as handle:
+            unserialized_data = pickle.load(handle)
+        return unserialized_data
+    except OSError:
+        print('cannot load: ', file_path)
+
+
+def load_txt_to_dict() -> dict:
+    file_path = get_file_path_for_loading("Choose a word embedding file in .txt format")
+    embeddings_dict = {}
+    with open(file_path, 'r', encoding="utf-8") as f:
+        for line in tqdm(f.readlines(), desc="Creating dictionary: "):
+            values = line.split(" ")
+            word = values[0]
+            vector = np.asarray(values[1:], "float32")
+            embeddings_dict[word] = vector
+    return embeddings_dict
+
+
+def load_vocab_to_list() -> list:
+    file_path = get_file_path_for_loading("Choose a word list file in .txt format")
+
+    vocab_list = []
+    with open(file_path, 'r', encoding="utf-8") as f:
+        for line in tqdm(f.readlines(), desc="Creating vocab list: "):
+            values = line.split(" ")
+            word = values[1]
+            vocab_list.append(word.rstrip())
+    return vocab_list
+
+
+def write_list_to_file(list_to_save):
+    file_saver = get_file_saver_instance(".txt")
+    if file_saver is None:
+        print("Not Saved")
+        return
+    for item in list_to_save:
+        file_saver.write(str(item))
+        file_saver.write("\n")
+    print("File saved")
+
+
+def write_nested_list_to_file(list_to_save):
+    file_saver = get_file_saver_instance(".txt")
+    if file_saver is None:
+        print("Not Saved")
+        return
+    for item in list_to_save:
+        for i, value in enumerate(item):
+            file_saver.write(str(value))
+            if i+1 < len(item):
+                file_saver.write("\t")
+        file_saver.write("\n")
+    print("File saved")
